bb39feedea943fe32f8ddeb209b085694d015fd6
CodyAdam <cody.ad4@gmail.com>
2022-09-06 22:43:38 +0200

    feat: implement the UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,28 @@
+import { Card, Button, Input, Text, Checkbox } from '@nextui-org/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { trpc } from '../utils/trpc';
-import * as UI from '@nextui-org/react';
+import { useState } from 'react';
+import { inferMutationInput, trpc } from '../utils/trpc';
+
+const DEFAULT_URL: inferMutationInput<'shortLink.create'> = {
+  isPublic: false,
+  url: '',
+  slug: '',
+};
 
 const Home: NextPage = () => {
   const query = trpc.useQuery(['shortLink.getAllPublic']);
+  const mutation = trpc.useMutation('shortLink.create');
+  const [createState, setCreateState] = useState(DEFAULT_URL);
+
+  function handleSubmit() {
+    mutation.mutate(createState, {
+      onSuccess() {
+        setCreateState(DEFAULT_URL);
+        query.refetch();
+      },
+    });
+  }
 
   return (
     <>
@@ -14,20 +32,62 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <main className='flex flex-col items-center justify-center min-h-screen p-4 bg-zinc-800'>
-        {/* <h1 className='text-5xl md:text-[5rem] leading-normal font-extrabold text-zinc-900'>
-          <span className='text-purple-300'>cdy</span>.pw
-        </h1> */}
-        <div className='max-w-2xl min-w-[35rem]'>
-          <UI.Card>
-            <UI.Card.Header>Create a new link</UI.Card.Header>
-            <UI.Card.Body>
-              <UI.Input /> 
-            </UI.Card.Body>
-            <UI.Card.Footer>
-              <UI.Button>Submit</UI.Button>
-            </UI.Card.Footer>
-          </UI.Card>
+      <main className='flex flex-col items-center justify-center min-h-screen p-4'>
+        <div className='w-[25rem] flex flex-col gap-10'>
+          <Card className='bg-[rgb(15,15,16)]'>
+            <Card.Header>Create a new link</Card.Header>
+            <Card.Divider />
+            <Card.Body className='flex flex-col gap-3'>
+              <Input
+                clearable
+                label='Long URL'
+                placeholder='https://example.com'
+                type='url'
+                value={createState.url}
+                onChange={(newValue) => {
+                  setCreateState({ ...createState, url: newValue.target.value });
+                }}
+              />
+              <Input
+                clearable
+                label='Slug'
+                labelLeft='cdy.pw/'
+                value={createState.slug}
+                onChange={(newValue) => {
+                  setCreateState({ ...createState, slug: newValue.target.value });
+                }}
+              />
+              <Checkbox
+                color='gradient'
+                onChange={(isPublic) => {
+                  setCreateState({ ...createState, isPublic });
+                }}
+                size='sm'
+                isSelected={createState.isPublic}
+              >
+                Public
+              </Checkbox>
+            </Card.Body>
+            <Card.Divider />
+            <Card.Footer className='justify-end'>
+              <Button onClick={handleSubmit}>Create</Button>
+            </Card.Footer>
+          </Card>
+          {query.isSuccess && (
+            <Card className='bg-[rgb(15,15,16)]'>
+              <Card.Header>Public links</Card.Header>
+              <Card.Divider />
+              <Card.Body>
+                {query.data.map((link) => (
+                  <div key={link.id} className='flex items-center justify-between gap-2'>
+                    <Text>/{link.slug}</Text>
+                    <Text h3>&rarr;</Text>
+                    <Text>{link.url}</Text>
+                  </div>
+                ))}
+              </Card.Body>
+            </Card>
+          )}
         </div>
       </main>
     </>
