8bbd4e918b01b9d1dd9480af8bd95e3e9c015d50
CodyAdam <cody.ad4@gmail.com>
2022-09-06 16:02:32 +0200

    feat: api endpoint for slug fetching

---
+++ src/pages/api/url/[slug].ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../server/db/client"
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const slug = req.query["slug"];
+
+  if (!slug || typeof slug !== "string") {
+    res.statusCode = 404;
+    res.send(JSON.stringify({ message: "Please use a normalized url" }));
+    return;
+  }
+
+  const data = await prisma.shortLink.findUnique({
+    where: {
+      slug: slug,
+    },
+  });
+
+  if (!data) {
+    res.statusCode = 404;
+    res.send(JSON.stringify({ message: "Url not found" }));
+    return;
+  }
+
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "s-maxage=10000000, stale-while-revalidate");
+
+  return res.json(data);
+}
