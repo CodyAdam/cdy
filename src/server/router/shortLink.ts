d13e5564ad13bbfb9ea89dee8018431ce9a3accc
CodyAdam <cody.ad4@gmail.com>
2022-09-06 16:02:50 +0200

    feat: move the slug fetching on the next api

--- src/server/router/shortLink.ts
+++ src/server/router/shortLink.ts
@@ -2,27 +2,14 @@ import { createRouter } from "./context";
 import { z } from "zod";
 
 export const shortLinkRouter = createRouter()
-  .query("getFromSlug", {
-    input: z
-      .object({
-        slug: z.string(),
-      }),
-    resolve({ input, ctx }) {
-      return ctx.prisma.shortLink.findUnique({
-        where: {
-          slug: input.slug,
-        },
-      });
-    },
-  })
   .query("getAll", {
     async resolve({ ctx }) {
-      return await ctx.prisma.shortLink.findMany();
+      return ctx.prisma.shortLink.findMany();
     },
   })
   .query("getAllPublic", {
     async resolve({ ctx }) {
-      return await ctx.prisma.shortLink.findMany({ where: { isPublic: true } });
+      return ctx.prisma.shortLink.findMany({ where: { isPublic: true } });
     },
   }).mutation("create", {
     input: z
