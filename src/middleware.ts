1981b7f75ad818103dea45ec9d32dfbb6d39805e
CodyAdam <cody.ad4@gmail.com>
2022-09-12 11:08:42 +0200

    fix: encode instead of decode

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// http://localhost:3000/󠁮󠁳󠁰󠁬
 
-export async function middleware(req: NextRequest) {
-  const url = decodeURI(req.nextUrl.pathname);
+export async function middleware(req: NextRequest) {  
+  const url = encodeURI(req.nextUrl.href);
   const slug = url.split("/").pop();
 
   const fetchedData = await fetch(`${req.nextUrl.origin}/api/url/${slug}`);
